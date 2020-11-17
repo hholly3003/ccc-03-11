9c2dc017c41959eb45cf19ee7324570e66af3e8a
Holly <h.holly3003@gmail.com>
2020-11-18 00:47:16 +1100

    refactored code for factory pattern

--- main.py
+++ main.py
@@ -2,23 +2,30 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from flask import Flask, jsonify
-app = Flask(__name__)
-app.config.from_object("default_settings.app_config")
-
-from database import init_db
-db = init_db(app)
-
+from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-ma = Marshmallow(app)
 
-from commands import db_commands
-app.register_blueprint(db_commands)
+db = SQLAlchemy()
+ma = Marshmallow()
 
-from controllers import registerable_controllers
-for controller in registerable_controllers:
-    app.register_blueprint(controller)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("default_settings.app_config")
 
-from marshmallow.exceptions import ValidationError
-@app.errorhandler(ValidationError)
-def handle_bad_request(error):
-    return (jsonify(error.messages), 400)
+    #Registering the db and ma into the app using init_app function
+    db.init_app(app)
+    ma.init_app(app)
+
+    from commands import db_commands
+    app.register_blueprint(db_commands)
+
+    from controllers import registerable_controllers
+    for controller in registerable_controllers:
+        app.register_blueprint(controller)
+
+    from marshmallow.exceptions import ValidationError
+    @app.errorhandler(ValidationError)
+    def handle_bad_request(error):
+        return (jsonify(error.messages), 400)
+
+    return app
